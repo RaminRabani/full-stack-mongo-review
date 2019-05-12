3caf4ebe67b0d0cbc009800f5d5c38bb70450c46
Ramin Rabani <rrabani2@gmail.com>
2019-05-11 17:09:30 -0700

    Render App

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const App = (props) => {
+  return (
+    <div>hello from React</div>
+  )
+}
+
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div>Hello assholes</div>
+//     )
+//   }
+// }
+
+export default App;
